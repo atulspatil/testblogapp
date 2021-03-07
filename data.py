873fe8b1044a28b7b41bb022a6c2fa18e4aca3fb
atul <atulp1012@gmail.com>
2021-03-07 19:05:22 +0530

    First commit for test blog app

---
+++ data.py
@@ -0,0 +1,18 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article one',
+            'body' : 'random things here',
+            'author': 'Brad Traversy',
+            'create_date': '1-10-2021'
+        },
+        {
+            'id': 2,
+            'title': 'Article two',
+            'body': 'more random things here',
+            'author': 'Traversy Media',
+            'create_date': '2-10-2021'
+        }
+    ]
+    return articles
